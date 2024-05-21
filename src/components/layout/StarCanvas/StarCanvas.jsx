132a98da10c38c2bc2683094e278088389e8fbd4
jonghyeon <whdgusdldl22@naver.com>
2024-05-21 14:25:51 +0900

    버그 수정

--- src/components/layout/StarCanvas/StarCanvas.jsx
+++ src/components/layout/StarCanvas/StarCanvas.jsx
@@ -5,15 +5,15 @@ import { Canvas, useFrame } from "@react-three/fiber";
 const StarCanvas = () => {
   const ref = useRef();
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.x -= delta / 20;
+    ref.current.rotation.y -= delta / 80;
   });
   return (
     <group>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
       <pointLight position={[-10, -10, -10]} />
-      <Stars ref={ref} radius={200} count={3000} />
+      <Stars ref={ref} radius={150} count={3000} />
     </group>
   );
 };
